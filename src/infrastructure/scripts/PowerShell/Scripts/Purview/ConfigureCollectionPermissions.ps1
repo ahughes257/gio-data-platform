d7f7f8536c2f7cad77309f73fd9c405cf604470c
mark_ <mark_@DESKTOP-2SAOQDO>
2024-01-10 11:37:40 +0000

    Updated for script consistency

--- src/infrastructure/scripts/PowerShell/Scripts/Purview/ConfigureCollectionPermissions.ps1
+++ src/infrastructure/scripts/PowerShell/Scripts/Purview/ConfigureCollectionPermissions.ps1
@@ -18,17 +18,17 @@ foreach ($file in $jsonFiles) {
   Write-Host $file.FullName
   $config = Get-Content $file.FullName 
 
-  foreach ($collection in $config.Collections) 
+  foreach ($collection in $config.collections) 
   {
-      foreach ($permission in $collection.Permissions) 
+      foreach ($permission in $collection.permissions) 
       {      
-        foreach($permissionGroup in $permission.GroupNames)   
+        foreach($permissionGroup in $permission.groupNames)   
         {
           Write-Host "Updating Policy for $permissionGroup"
-          $policy = Get-PurviewPolicyByCollectionName -AccessToken $AccessToken -CollectionName $collection.Name -ApiVersion '2021-07-01-preview' -BaseUri $baseUrl
+          $policy = Get-PurviewPolicyByCollectionName -AccessToken $AccessToken -CollectionName $collection.name -ApiVersion '2021-07-01-preview' -BaseUri $baseUrl
          
           #Assign a group to a role
-          Add-PurviewPolicyRole -AccessToken $AccessToken -BaseUri $baseUrl -ApiVersion '2021-07-01-preview' -Policy $policy.values[0] -RoleName $permission.Group -GroupId $permissionGroup -CollectionName $collection.Name
+          Add-PurviewPolicyRole -AccessToken $AccessToken -BaseUri $baseUrl -ApiVersion '2021-07-01-preview' -Policy $policy.values[0] -RoleName $permission.group -GroupId $permissionGroup -CollectionName $collection.name
           Write-Host "Added group with id $permissionGroup to the $permissionGroup role"
         }
       }
